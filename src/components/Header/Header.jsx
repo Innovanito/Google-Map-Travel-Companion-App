119ffb3b761164836f88edf3f46e0c3911e14ad1
김하경 <khg05311@naver.com>
2022-01-05 16:41:18 +0900

    25:50

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { Autocomplete } from '@react-google-maps/api'
+import {AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core'
+import searchIcon from '@material-ui/icons/Search'
+
+
 
 const Header = () => {
   return (
     <div>
-      header
+      <AppBar position='static'>
+        <Toolbar className={classes.toolbar} >
+          <Typography variant='h5' className={classes.title}>
+            Travel Advisor
+          </Typography>
+          <Box display='flex'>
+            <Typography variant='h5' className={classes.title}>
+              Travel Advisor
+            </Typography>
+            <Autocomplete>
+              <div className={classes.search}>
+                <div className={classes.searchIcon}>
+                  <SearchIcon/>
+                </div>
+                <InputBase placeholder='Search ...' classes={{ root: classes.inputRoot, input: classes.inputInput}} />
+              </div>
+            </Autocomplete>
+          </Box>
+        </Toolbar>
+      </AppBar>
     </div>
   )
 }
